4babc2df863ff846ddf0cf36e140ce447b4d91bb
Tyson Crosbie <tyson@tysoncrosbie.com>
2024-09-23 15:37:42 -0700

    Add external Api to Search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import './App.css';
+import {ChangeEvent, SyntheticEvent, useState} from "react";
+import "./App.css";
 import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
 import Navbar from "./Components/Navbar/Navbar";
+import {searchCompanies} from "./api";
+import {CompanySearch} from "./company";
+
 
 function App() {
+    const [search, setSearch] = useState<string>('');
+    const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+    const [serverError, setServerError] = useState<string | null>(null);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    };
+
+    const onClick = async (e: SyntheticEvent) => {
+        const result = await searchCompanies(search);
+
+        if (typeof result === "string") {
+            setServerError(result);
+        } else if (Array.isArray(result.data)) {
+            setSearchResult(result.data);
+        }
+
+        console.log(searchResult);
+    };
+
     return (
-        
-    <div className="App">
-        <Navbar/>
-        <CardList/>
-    </div>
-)
+
+        <div className="App">
+            <Navbar/>
+            <Search onClick={onClick} search={search} handleChange={handleChange}/>
+            <CardList/>
+        </div>
+    )
 }
 
 export default App;
